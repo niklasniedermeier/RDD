c922fd8cd4a05d81c620884d294a87a8f1b53ff1
Niklas Niedermeier <niedermeier.niklas@web.de>
2024-12-18 18:26:11 +0100

    Split mrot into mulitple functions to increase modularization.

--- functions/mrot.R
+++ functions/mrot.R
@@ -1,8 +1,4 @@
-mrot <- function(X, Y, method, c = NULL, p = 3, nknots = 3, show_plot = FALSE) {
-  
-  if (p == 1) {
-    return(0)
-  }
+mrot <- function(X, Y, method, c = NULL, p = 3, nknots = 3) {
   
   if (is.null(c)){
     list_df <- list(
@@ -15,10 +11,6 @@ mrot <- function(X, Y, method, c = NULL, p = 3, nknots = 3, show_plot = FALSE) {
     ) 
   }
   
-  #if (show_plot){
-  #  list_df_plot <- list()
-  #}
-  
   f2_maxima <- c()
 
   for (i in 1:length(list_df)){
@@ -26,154 +18,121 @@ mrot <- function(X, Y, method, c = NULL, p = 3, nknots = 3, show_plot = FALSE) {
      df <- list_df[[i]]
      
      if (method == "poly"){
-       
-       model      <- lm(df$Y ~ poly(df$X, p, raw = TRUE))
-       model_coef <- coef(model)
-       
-       f2_max_i <- optimize(
-         function(x) f2_poly(x, coef = model_coef, p = p), 
-         interval = range(df$X), 
-         maximum = TRUE
-       )$objective
-       
-       f2_maxima[i] <- f2_max_i
+       f2_maxima[i] <- mrot_poly(X = df$X, Y = df$Y, p = p)
      }
      
-     if (method == "Imbens"){
-       
-       poly_factor = 2
-       model      <- lm(df$Y ~ poly(df$X, p, raw = TRUE))
-       model_coef <- coef(model)
-       
-       f2_max_i <- optimize(
-         function(x) f2_poly(x, coef = model_coef, p = p), 
-         interval = range(df$X), 
-         maximum = TRUE
-       )$objective
-       
-       f2_maxima[i] <- poly_factor * f2_max_i
+     if (method == "poly_2_se"){
+       f2_maxima[i] <- mrot_poly_2_se(X = df$X, Y = df$Y)
      }
      
-     if (method == "smooth_spline"){
-       
-       model <- npreg::ss(
-         x = df$X , y = df$Y,
-         method = "BIC",
-         nknots = nknots
-       )
-       
-       f2_max_i <- npreg::predict.ss(model,df$X, deriv = 2)$y %>% abs() %>% max()
-       
-       f2_maxima[i] <-  f2_max_i 
+     if (method == "ss"){
+       f2_maxima[i] <- mrot_ss(X = df$X, Y = df$Y, nknots = nknots)
      }
      
      if (method == "spline"){
-       
-       knot_seq  <- seq(from = min(df$X), to = max(df$X), len = nknots)
-       
-       X_mat     <- splines2::bSpline(
-         x              = df$X, 
-         knots          = knot_seq[-c(1, length(knot_seq))], 
-         degree         = p,
-         intercept      = TRUE, 
-         Boundary.knots = knot_seq[ c(1, length(knot_seq))]
-       )
-       
-       beta_hat <- solve(t(X_mat) %*% X_mat)  %*% t(X_mat)  %*% df$Y
-       
-       X_smooth_length <- min(length(df$X), 100)
-       
-       X_smooth <- seq(min(df$X),max(df$X), length.out = X_smooth_length)
-       
-       X_mat_smooth <- splines2::bSpline(
-         x              = X_smooth, 
-         knots          = knot_seq[-c(1, length(knot_seq))], 
-         degree         = p,
-         intercept      = TRUE, 
-         Boundary.knots = knot_seq[ c(1, length(knot_seq))]
-       )
-       
-       Y_smooth_hat <- X_mat_smooth %*% beta_hat
-       
-       f2_maxima[i] <- numerical_max_second_derivative(X = X_smooth, Y = Y_smooth_hat)
+       f2_maxima[i] <- mrot_spline(X = df$X, Y = df$Y, nknots = nknots, p = p)
      }
 
   }
-  
-  #if (show_plot){
-  #  list_df_plot[[i]] <- data.frame(
-  #    X = pred$x,
-  #    Y = pred$y
-  #  ) %>% arrange(X)
-  #}
-  
-  #if (show_plot){
-  #  plot(X, Y, type = "p", col = "black", xlim = c(-1, 1), ylim = c(-1, 2))
-  #  for (i in 1:length(list_df)){
-  #    lines(list_df_plot[[i]]$X, list_df_plot[[i]]$Y, col = "red")
-  #  }
-  #  title(main = paste("M = ",round(f2_max,2)))
-  #}
+
   
   f2_max <- round(max(f2_maxima),4)
   
   return(f2_max)
 }
 
+
+mrot_poly <- function(X, Y, p){
+  
+  if (p == 1){return(0)}
+  
+  model      <- lm(Y ~ poly(X, p, raw = TRUE))
+  model_coef <- coef(model)
+  
+  if (p == 2){
+    f2_max <- abs(2 * model_coef[[3]])
+  }
+  
+  if (p > 2){
+    f2_max <- optimize(
+      function(x) f2_poly(x, coef = model_coef, p = p), 
+      interval = range(X), 
+      maximum = TRUE
+    )$objective 
+  }
+  
+  return(f2_max)
+}
+
+mrot_poly_2_se <- function(X, Y){
+  
+  model      <- lm(Y ~ poly(X, p = 2, raw = TRUE))
+  model_coef <- coef(model)
+  
+  f2_max <- abs(2 * model_coef[[3]])
+  
+  leading_se <- summary(model)$coefficients[, "Std. Error"][[3]]
+
+  delta_upper_ci <- leading_se * 1.96
+  f2_max <- f2_max + delta_upper_ci
+  
+  f2_max <- f2_max #* 1.8
+  
+  return(f2_max)
+}
+
+
+mrot_ss <- function(X, Y, nknots, p = p){
+  model <- npreg::ss(
+    x = X , y = Y,
+    method = "BIC",
+    nknots = nknots
+  )
+  
+  f2_max <- npreg::predict.ss(model, X, deriv = 2)$y %>% abs() %>% max()
+  
+  return(f2_max)
+}
+
+
+mrot_spline <- function(X, Y, nknots, p){
+  
+  knot_seq  <- seq(from = min(X), to = max(X), len = nknots)
+  
+  X_mat     <- splines2::bSpline(
+    x              = X, 
+    knots          = knot_seq[-c(1, length(knot_seq))], 
+    degree         = p,
+    intercept      = TRUE, 
+    Boundary.knots = knot_seq[ c(1, length(knot_seq))]
+  )
+  
+  beta_hat <- solve(t(X_mat) %*% X_mat)  %*% t(X_mat)  %*% Y
+  
+  X_smooth_length <- min(length(X), 100)
+  
+  X_smooth <- seq(min(X), max(X), length.out = X_smooth_length)
+  
+  X_mat_smooth <- splines2::bSpline(
+    x              = X_smooth, 
+    knots          = knot_seq[-c(1, length(knot_seq))], 
+    degree         = p,
+    intercept      = TRUE, 
+    Boundary.knots = knot_seq[ c(1, length(knot_seq))]
+  )
+  
+  Y_smooth_hat <- X_mat_smooth %*% beta_hat
+  
+  f2_max <- numerical_max_second_derivative(X = X_smooth, Y = Y_smooth_hat)
+  
+  return(f2_max)
+}
+
+
 f2_poly <- function(x, coef, p) {
   abs(sum(sapply(2:p, function(k) k * (k - 1) * coef[k + 1] * x^(k - 2))))
 }
 
-
-
-mrot_update <- function(Y, X, method, c, p = 3, nknots = 3, rd_params = NULL, method_update = NULL, runs = 1, show_plot = FALSE) {
-  
-  if (is.null(method_update)){
-    method_update = method
-  }
-  
-  if (is.null(rd_params)){
-    rd_params <- data.frame(
-      kernel = "triangular",
-      ci_method = "honest",
-      bw_method = "MSE",
-      bw_method_uniform = TRUE,
-      se_method = "nn",
-      alpha = 0.05
-    )
-  }
-  
-  # Get initial m estimate
-  m_hat <- mrot(X, Y, method, c, p, nknots, show_plot)
-  
-  for (i in 1:runs){
-    
-    # Get RD estimates for initial tau
-    rd_estimates <- suppressMessages(
-      rd(
-        Y = Y,
-        X = X,
-        cutoff = c,
-        M = m_hat,
-        kernel = rd_params$kernel,
-        ci_method = rd_params$ci_method,
-        bw_method = rd_params$bw_method,
-        bw_method_uniform = rd_params$bw_method_uniform,
-        se_method = rd_params$se_method,
-        alpha = rd_params$alpha
-      )
-    )
-    
-    tau_hat <- rd_estimates$tau_hat
-    
-    # Adjust Y's below the cutoff using our estimate for tau
-    Y[X>c] <- Y[X>c] - tau_hat
-    c <- NULL
-    m_hat <- mrot(X, Y, method_update, c, p, nknots, show_plot)
-  }
-  
-  return(m_hat)
-}
 
 numerical_max_second_derivative <- function(X,Y) {
   
@@ -189,6 +148,7 @@ numerical_max_second_derivative <- function(X,Y) {
 
   return(max(abs(data$f2)))
 }
+
 
 optimize_polynomial_degree <- function(x, y, max_degree = 10, n_folds = 5) {
   
